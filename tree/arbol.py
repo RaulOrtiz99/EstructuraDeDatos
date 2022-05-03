326930ebc9bcb0a25f635ff93075b34fe074d4f2
Raul Alberto Ortiz M <36978009+RaulOrtiz99@users.noreply.github.com>
2022-05-02 23:08:45 -0400

    agregando metodos de un arbol binario, la idea es realizarlos esta semana

--- tree/arbol.py
+++ tree/arbol.py
@@ -6,3 +6,14 @@ class Arbol:
     def __init__(self,valor):
         self.raiz= None 
         pass
+
+    #METODOS DE LOS ARBOLES BINARIOS
+
+    #INSERTAR -> insertar un valor en el arbol binario
+    #ES VACIO -> Devuelte TRUE si esta vacio
+    #Es HOJA -> Devuelve TRUE si el arbol no tiene ni hijo izquierdo ni hijo derecho eso quiere decir que es hoja
+    #BUSCAR -> Busca un nodo por su valor
+    #RECORRIDO POST ORDEN
+    #RECORRIDO PRE ORDEN
+    #RECORRIDO INORDEN
+    #METODO ES RAMA -> Devuelve verdadero si es una rama
